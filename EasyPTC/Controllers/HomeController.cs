1cb43f42be938f8722e79b5a68c708bf475c1914
Teodora Cholakova <tchol@softserveinc.com>
2018-12-18 09:47:45 +0200

    Delete the unnecessary usings.

--- EasyPTC/Controllers/HomeController.cs
+++ EasyPTC/Controllers/HomeController.cs
@@ -1,12 +1,7 @@
-﻿using EasyPTC.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace EasyPTC.Controllers
+﻿namespace EasyPTC.Controllers
 {
+    using System.Web.Mvc;
+
     public class HomeController : Controller
     {
         public ActionResult Index()
