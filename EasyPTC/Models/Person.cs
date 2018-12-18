fd493630d8dafe27034ddb80d4adb2fd8c571be9
Teodora Cholakova <tchol@softserveinc.com>
2018-12-18 13:50:01 +0200

    Remove namespaces.

--- EasyPTC/Models/Person.cs
+++ EasyPTC/Models/Person.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace EasyPTC.Models
+﻿namespace EasyPTC.Models
 {
     public class Person
     {
+        public string Name { get; set; }
+        public int Age { get; set; }
     }
 }
